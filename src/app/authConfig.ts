36b9aaa9f7054fb87cbd8cf626a2ba1366397e70
Rigers Ramushi <rramushi@ritech.co>
2025-06-01 03:51:11 +0200

    t

--- src/app/authConfig.ts
+++ src/app/authConfig.ts
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: "ac2caf92-d690-42d9-a731-2eaceaa9fb74",
-    authority: "https://login.microsoftonline.com/b1fbc6e5-5c4b-45bd-ad4c-4556e0b49d61",
+    authority: "https://login.microsoftonline.com/a20cc188-b4b8-4f19-97e6-3e1724e52d1c",
     redirectUri: "https://rigersramushi.github.io/"
   },
   cache: {
